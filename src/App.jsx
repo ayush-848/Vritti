ab7d299f0dc0c887a98645038cd57f4bf24db80a
Ayush Deb <ayushdeb848@gmail.com>
2024-10-07 14:59:05 +0530

    fixed leaderboard bug

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,3 @@
-// App.js
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import Home from './components/Home'
@@ -8,12 +7,12 @@ import Navbar from './components/Navbar';
 const App = () => {
   return (
     <>
-    
-      <Navbar/>
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/leaderboard" element={<Leaderboard />} />
-    </Routes></>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/leaderboard" element={<Leaderboard />} />
+      </Routes>
+    </>
   );
 };
 
